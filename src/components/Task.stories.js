ce9cd99c11223c82c9d30503466a65b19399e10c
Alex Nicholas <me@alexnicholas.dev>
2022-02-15 17:12:57 +0000

    add regression test for textOverflow

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -30,3 +30,12 @@ Archived.args = {
     state: "TASK_ARCHIVED",
   },
 }
+
+export const LongTitle = Template.bing({})
+LongTitle.args = {
+  task: {
+    ...Default.args.task,
+    title:
+      "Hey look! This is a really long title and we are using this to see if the text overflow ellipsis works as we would hope. This ensures that we can implement a regression test. Is it a visual regression test? Maybe, we'll see!",
+  },
+}
